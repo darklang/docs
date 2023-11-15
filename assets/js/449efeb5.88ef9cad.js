7038ff0bbbc60b4410a53afb3f738f1c4b81d53f
Dark Ops <ops@darklang.com>
2023-11-15 03:11:48 +0000

    [skip ci] Automated Deploy of d53962ff9e7e0a90415fe08b4a196d858fe36b9b via CircleCI

---
+++ assets/js/449efeb5.88ef9cad.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[3960],{86808:s=>{s.exports=JSON.parse('{"title":"Discussions","slug":"/category/discussion","permalink":"/next/category/discussion","navigation":{"previous":{"title":"First How-to","permalink":"/next/how-to/base"},"next":{"title":"First Discussion","permalink":"/next/discussion/base"}}}')}}]);
